4d8ab42f7d5ea32df9dfcff25e08c263a5293202
yooodleee <accia25@naver.com>
2025-06-20 18:17:33 +0900

    add user account

---
+++ system/add-user/adduser-script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+echo "사용자 계정 생성"
+echo "======================="
+
+
+# 사용자 계정 및 패스둬으가 입력되었는지 확인
+if [[ -n $1 ]] && [[ -n $2 ]]
+then
+
+  UserList=($1)
+  Password=($2)
+
+  # for 문을 이용하여 사용자 계정 생성
+  for (( i=0; ${UserList[@]}; i++ ))
+  do
+    # if 문을 사용하여 사용자 계정이 있는지 확인
+    if [[ $(cat /etc/passwd | grep ${UserList[$i]} | wc -l) == 0 ]]
+    then
+      # 사용자 생성 및 패스워드 설정
+      useradd ${UserList[$i]}    
+      echo ${Password[$i]} | passwd ${UserList[$i]} --stdin
+    else
+      # 사용자가 있다고 메시지를 보여줌
+      echo "this user ${Userlist[$i]} is existing."
+    fi
+  done
+
+else
+  # 사용자 계정과 패스워드를 입력하라는 메시지를 보여줌
+  echo -e 'Please input user id and password.\nUsage: adduser-script.sh "user01 user02" "pw01 pw02"'
+fi

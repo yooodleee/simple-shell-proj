f40cd3a8cc7a2b64bc2719f68b522bdd32a0365f
yooodleee <accia25@naver.com>
2025-06-20 19:15:46 +0900

    generate ssh key and copy multi-server

---
+++ system/ssh-key/send-new-ssh-key.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+echo "ssh key를 여러 서버에 복사"
+echo "======================="
+
+
+# 접속할 서버 정보, SSH 키 경로, 공개 키 경로를 변수에 저장
+servers="host01 host02"
+sshKey="$HOME/.ssh/key.pem"
+sshPub="$HOME/.ssh/key/pem.pub"
+
+# SSH Key 생성
+ssh-keygen -q -N "" -f $sshKey
+
+# 생성된 SSH Key를 해당 서버에 복사
+for server in $servers
+do
+  echo $server
+  sshpass -p "$1" ssh-copy-id -i $sshPub stack@server
+done
